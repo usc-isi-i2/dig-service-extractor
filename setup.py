a6ea686247d7d8be9079d8926374c9eb52d52b01
ZwEin <zwein27@gmail.com>
2016-10-06 23:14:44 -0700

    fix typo

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 # @Author: ZwEin
 # @Date:   2016-09-30 14:01:47
 # @Last Modified by:   ZwEin
-# @Last Modified time: 2016-10-06 22:48:18
+# @Last Modified time: 2016-10-06 23:14:36
 
 
 from distutils.core import setup
@@ -10,9 +10,9 @@ from setuptools import Extension,find_packages
 from os import path
 
 setup(
-    name = 'digServiceDateExtractor',
+    name = 'digServiceExtractor',
     version = '0.1.0',
-    description = 'digServiceDateExtractor',
+    description = 'digServiceExtractor',
     author = 'Lingzhe Teng',
     author_email = 'zwein27@gmail.com',
     url = 'https://github.com/usc-isi-i2/dig-service-extractor',
