924326f179c423554def0a64e7a3bd8e2f7115a2
ZwEin <zwein27@gmail.com>
2016-10-06 23:15:56 -0700

    fix service  keyword

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 # @Author: ZwEin
 # @Date:   2016-09-30 14:01:47
 # @Last Modified by:   ZwEin
-# @Last Modified time: 2016-10-06 23:14:36
+# @Last Modified time: 2016-10-06 23:15:43
 
 
 from distutils.core import setup
@@ -18,6 +18,6 @@ setup(
     url = 'https://github.com/usc-isi-i2/dig-service-extractor',
     download_url = 'https://github.com/usc-isi-i2/dig-service-extractor',
     packages = find_packages(),
-    keywords = ['age', 'extractor'],
+    keywords = ['service', 'extractor'],
     install_requires=['digExtractor', 'digCrfTokenizer', 'digDictionaryExtractor']
     )
